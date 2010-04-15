6f80f12c2e6165a1f2680b946853a971189e709e
kosz.storage <kosz.storage@1ff8e794-542d-11de-907f-3386865dedc4>
2010-04-15 04:22:48 +0000

    Version for redmine 0.9.x
    git-svn-id: http://kosz-redmine-tools.googlecode.com/svn/trunk/plugins/redmine_issue_history_tabs@8 1ff8e794-542d-11de-907f-3386865dedc4

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_issue_history_tabs do
   name 'Redmine Issue History Tabs plugin'
   author 'Konstantin Zaitsev'
   description 'This plugin provide history of issue in tabs representation'
-  version '0.0.1'
+  version '0.0.2'
 end
